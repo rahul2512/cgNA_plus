7c3b73b99600fa97bd1b6f3c89b2831a55e05711
rahul2512 <rs25.iitr@gmail.com>
2022-11-18 09:23:19 +0100

    remove main.py and added example

--- Examples/example_1.py
+++ Examples/example_1.py
@@ -3,9 +3,14 @@ import sys, random, re
 sys.path.append('./../classes')
 sys.path.append('./../modules')
 from cgDNAclass import cgDNA
+from cgDNAUtils import *
 
 
 seq1 = "GCATCATCAGACTACTACTCAGC"  # randomly chosen sequence
-
 data1 = cgDNA(seq1,"dna_ps2")
+data2 = cgDNA(seq1,"rna_ps2")
+data3 = cgDNA(seq1,"drh_ps2")
 
+gs = GrooveWidths_CS(data1.ground_state)
+
+print("Groovewidth (minor and major) for ",seq1," is ", gs)
